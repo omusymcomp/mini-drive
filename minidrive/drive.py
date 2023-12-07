370606d4e2ab72bcd461c8a0b2292e002c3c4730
sugi <sd23080r@omu.ac.jp>
2023-12-07 20:19:53 +0900

    initialize関数とcleanup関数を定義

--- minidrive/drive.py
+++ minidrive/drive.py
@@ -1,5 +1,16 @@
+import RPi.GPIO as GPIO
+IN1 = 13
+IN2 = 0
+NSLP = 12
+
 def initialize():
-    print('ini')
+    GPIO.setmode(GPIO.BCM)
+    for i in [IN1, IN2, NSLP]:
+        GPIO.setup(i, GPIO.OUT)
+        GPIO.output(i, GPIO.LOW)
+    pi = GPIO.PWM(IN1, 100)
+    pi.start(0)
+    GPIO.output(NSLP, 1)
 
 def set_duty():
     print("set duty")
@@ -8,4 +19,4 @@ def short():
     print("short_brake")
 
 def cleanup():
-    print("cleaned up")
+    GPIO.cleanup()
